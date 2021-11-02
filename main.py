c1380e487e1567adece1f322a24def7ccb31b34b
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-11-02 17:01:17 +0100

    add str check

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from jsonschema import validate
 canIDsSchema = {
     "Boards": [
         {
-            "ID": "0x01",
+            "ID": "number",
             "Name": "PiBox"
         },
         {
@@ -35,9 +35,11 @@ def isnumericbase(s, base):
 def ishexadecimalstring(s):
     return isnumericbase(s, 16)
 
-
 # Convert json to python object.
-my_json = json.load(open('canIDs.json',))
+
+
+my_json = json.load(open('canIDs.json', ))
+
 
 # Validate will raise exception if given json is not
 # what is described in schema.
@@ -45,12 +47,23 @@ validate(instance=my_json, schema=canIDsSchema)
 
 # print for debug
 # print(my_json)
-# print(my_json['Boards'][0]['ID'])
 
+
+# checking id_number type
 for i in range(len(my_json['Boards'])):
     id_number = my_json['Boards'][i]['ID']
     if ishexadecimalstring(id_number):
         print(id_number, "is hexadecimal")
+    else:
+        print(id_number, "is not")
 
 for i in range(len(my_json['Parameters'])):
     print(my_json['Parameters'][i])
+
+# checking name type
+for i in range(len(my_json['Boards'])):
+    name_n = my_json['Boards'][i]['Name']
+    if isinstance(name_n, str):
+        print(my_json['Boards'][i]['Name'], "is correct")
+    else:
+        print(my_json['Boards'][i]['Name'], "is not correct")

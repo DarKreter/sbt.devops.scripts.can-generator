7f2977b86937130fbc84b440acc0c59ae3d13ea5
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-11-19 13:54:40 +0100

    removed functions replaced in schema

--- main.py
+++ main.py
@@ -85,50 +85,11 @@ canIDsSchema = {
 }
 
 
-def isnumericbase(s, base):
-    try:
-        int(s, base)
-        return True
-    except ValueError:
-        return False
-
-
-def ishexadecimalstring(s):
-    return isnumericbase(s, 16)
-
-
 # Convert json to python object.
 my_json = json.load(open(args.json_file))
 # Validate will raise exception if given json is not
 # what is described in schema.
 validate(instance=my_json, schema=canIDsSchema)
-
-# BOARDS CHECKING
-# checking id_number type
-for i in range(len(my_json['Boards'])):
-    id_number = my_json['Boards'][i]['ID']
-    if ishexadecimalstring(id_number):
-        continue
-        # print(id_number, "- is hexadecimal")
-    else:
-        print("error", my_json['Boards'][i]['ID'])
-        # print(id_number, "- is not")
-
-
-# checking name type in Boards
-for i in range(len(my_json['Boards'])):
-    name_n = my_json['Boards'][i]['Name']
-    if isinstance(name_n, str):
-        continue
-        # print("Boards_Name:", my_json['Boards'][i]['Name'], "- is correct")
-    else:
-        print("error", my_json['Boards'][i]['Name'])
-        # print("Boards_Name:", my_json['Boards'][i]['Name'], "- is not correct")
-
-
-print("\n")
-
-
 # PARAMETERS CHECKING
 # looking for name in Parameters
 for i in range(len(my_json['Parameters'])):

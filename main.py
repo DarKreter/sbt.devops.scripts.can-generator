add020a78d19d9fe32d80ffa520a5719b6eade82
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-11-23 18:56:44 +0100

    schema moved to separate file

--- main.py
+++ main.py
@@ -1,101 +1,35 @@
 import json
-from jsonschema import validate
+from jsonschema import validate, Draft7Validator
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("json_file", type=str, help="input JSON filename with extention")
+parser.add_argument("schema", type=str, help="input JSON filename with schema")
 args = parser.parse_args()
 
-canIDsSchema = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "Boards": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "ID": {
-            "type": "string",
-            "pattern": "^(0[xX])[A-Fa-f0-9]+$"
-          },
-          "Name": {
-            "type": "string"
-          }
-        },
-        "required": [
-          "ID",
-          "Name"
-        ]
-      }
-    },
-    "Parameters": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "ID": {
-            "type": "string",
-            "pattern": "^(0[xX])[A-Fa-f0-9]+$"
-          },
-          "Name": {
-            "type": "string"
-          },
-          "Type": {
-            "type": "string",
-            "enum": [
-              "uint32",
-              "bool",
-              "float"
-            ]
-          },
-          "MinID": {
-            "type": "string",
-            "pattern": "^(0[xX])[A-Fa-f0-9]+$"
-          },
-          "MaxID": {
-            "type": "string",
-            "pattern": "^(0[xX])[A-Fa-f0-9]+$"
-          }
-        },
-        "oneOf": [
-          {
-            "required": [
-              "ID",
-              "Name",
-              "Type"
-            ]
-          },
-          {
-            "required": [
-              "MinID",
-              "MaxID",
-              "Name",
-              "Type"
-            ]
-          }
-        ],
-      }
-    }
-  },
-  "required": [
-    "Boards",
-    "Parameters"
-  ]
-}
-
-
 # Convert json to python object.
-my_json = json.load(open(args.json_file))
+with open(args.json_file) as to_check:
+    my_json = json.load(to_check)
+with open(args.schema) as to_schema:
+    my_schema = json.load(to_schema)
 # Validate will raise exception if given json is not
 # what is described in schema.
-validate(instance=my_json, schema=canIDsSchema)
-# PARAMETERS CHECKING
-# looking for name in Parameters
-for i in range(len(my_json['Parameters'])):
-    print("Parameters_Name:", my_json['Parameters'][i]["Name"])
-    # if not print("ID:", my_json['Parameters'][i]['ID']):
-    # if na sprawdzenie czy min i max są
-    # print("MinID:", my_json['Parameters'][i]['MinID'])
-    # print("MaxID:", my_json['Parameters'][i]['MaxID'])
-    print("Type:", my_json['Parameters'][i]["Type"])
+validate(instance=my_json, schema=my_schema)
+
+
+def check_parameters():
+    # PARAMETERS CHECKING
+    # looking for name in Parameters
+    for i in range(len(my_json['Parameters'])):
+        print("Parameters_Name:", my_json['Parameters'][i]["Name"])
+        print("Type:", my_json['Parameters'][i]["Type"])
+
+
+# check weather MinID is smaller than MaxID
+def compare_min_and_max(Min, Max):
+    if int(Min, 16) >> int(Max, 16):
+        print("Wrong MinID and MaxID input")
+
+
+for j in my_json:
+    compare_min_and_max(my_json['Parameters'][2]['MinID'], my_json['Parameters'][2]["MaxID"])

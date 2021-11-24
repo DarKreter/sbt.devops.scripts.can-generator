a2a0610f3aea593840acd48f39288b743cd12fa3
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-11-24 21:32:20 +0100

    unique name check

--- main.py
+++ main.py
@@ -34,3 +34,29 @@ def compare_min_and_max(Min, Max):
 for j in range(len(my_json)):
     if 'ID' not in my_json['Parameters'][j]:
         compare_min_and_max(my_json['Parameters'][j]['MinID'], my_json['Parameters'][j]["MaxID"])
+
+
+def not_double_name():
+    name_board = []
+    name_parameters = []
+    for i in range(len(my_json["Boards"])):
+        name_board.append(my_json["Boards"][i]["Name"])
+    for i in range(len(my_json["Parameters"])):
+        name_parameters.append(my_json["Parameters"][i]["Name"])
+    name_board.sort()
+    name_parameters.sort()
+    for i in range(len(name_board)):
+        if i < len(name_board) - 1:
+            if name_board[i] == name_board[i + 1]:
+                print("boards name error")
+    for i in range(len(name_parameters)):
+        if i < len(name_parameters) - 1:
+            if name_parameters[i] == name_parameters[i + 1]:
+                print("parameters name error")
+
+
+not_double_name()
+
+
+# parameters muszą mieć osobne pozycje, więc trzeba będize każdy obiekt "zapisac do osobnej zmiennej"
+# aby później generować plik hpp dla każdego osobnego parametru z indywidualnym ID

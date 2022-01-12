57a4431a4e0e0afe1a4a42f5b72bc43051acc0ad
somesoo <jakasperczak@gmail.com>
2022-01-12 02:01:58 -1000

    .h gen complete

--- headerGen.py
+++ headerGen.py
@@ -5,23 +5,54 @@ class HGenerate:
 	enum8 = ""
 	enum16 = ""
 	def __init__(self, indef_name):
-			self.name = indef_name
-			self.indef = f"#ifndef F1XX_PROJECT_TEMPLATE_LIB_SBT_SDK_F1XX_SBT_SDK_SYSTEM_{self.name}_H\n#define F1XX_PROJECT_TEMPLATE_LIB_SBT_SDK_F1XX_SBT_SDK_SYSTEM_{self.name}_H"
-			self.include = "\n\n#include <cstdint>\nnamespace SBT {\nnamespace System {\nnamespace Communication {"
-			self.enum8 = f"\n\nenum class CANBoardID : unit8_t"
-			self.enum16 = f"\n\nenum class CANParameterID : unit16_t"
+		self.name = indef_name
+		self.indef = f"#ifndef F1XX_PROJECT_TEMPLATE_LIB_SBT_SDK_F1XX_SBT_SDK_SYSTEM_{self.name}_H\n#define F1XX_PROJECT_TEMPLATE_LIB_SBT_SDK_F1XX_SBT_SDK_SYSTEM_{self.name}_H"
+		self.include = "\n\n#include <cstdint>\nnamespace SBT {\nnamespace System {\nnamespace Communication {"
+		self.enum8 = "\n\nenum class CANBoardID : unit8_t {\n  "
+		self.enum16 = "\n\nenum class CANParameterID : unit16_t {\n  "
 
-	
+	def id_range_loop(self, json_object, i, file, rng):
+		max_id = int(json_object[rng][i]['MaxID'], 16)
+		curr_id = int(json_object[rng][i]['MinID'], 16)
+		adresses = []
+		#print(type(max_id))
+		while(max_id >= curr_id):
+			adresses.append(hex(curr_id))
+			curr_id = curr_id + 1
+		x = 1
+		for j in range(len(adresses)):
+			file.write(json_object[rng][i]["Name"].replace("<x>", str(x)))
+			x += 1
+			file.write(" = ")
+			file.write(str(adresses[j]))
+			file.write("\n  ")
 
 	def write_to_file(self, file_name, json_object):
+		# check if file allready exist
+
 		with open(f"{file_name}", "w") as file:
 			file.write(self.indef)
 			file.write(self.include)
 			file.write(self.enum8)
 			# for loop
-			file.write("\n    }")
+			for i in range(len(json_object["Boards"])):
+				if 'ID' not in json_object['Boards'][i]:
+					self.id_range_loop(json_object, i, file, "Boards")
+				else:
+					file.write(json_object['Boards'][i]["Name"])
+					file.write(" = ")
+					file.write(json_object["Boards"][i]['ID'])
+				file.write("\n  ")
+			file.write("\n} // enum CanBoardsID")
 			file.write(self.enum16)
-			# for loop
-			file.write("\n    }")
+			for i in range(len(json_object["Parameters"])):
+				if 'ID' not in json_object['Parameters'][i]:
+					self.id_range_loop(json_object, i, file, "Parameters")
+				else:
+					file.write(json_object['Parameters'][i]["Name"])
+					file.write(" = ")
+					file.write(json_object["Parameters"][i]['ID'])
+				file.write("\n  ")
+			file.write("\n} // enum CanParameterID\n} //namespace SBT\n} // namespace System\n} // namespace Communication")
 
 
---
+++ runScript.py
@@ -0,0 +1,32 @@
+# TODO  parameters muszą mieć osobne pozycje, więc trzeba będize każdy obiekt "zapisac do osobnej zmiennej"
+#  aby później generować plik hpp dla każdego osobnego parametru z indywidualnym ID
+import argparse
+import jsonschema
+import json  # , Draft7Validator
+from checker_class import Checker
+from headerGen import HGenerate
+
+parser = argparse.ArgumentParser()
+parser.add_argument("json_file", type=str, help="input JSON filename with extension")
+parser.add_argument("schema", type=str, help="input JSON filename with schema")
+parser.add_argument("header_name", type=str, help="input new file name")
+args = parser.parse_args()
+
+
+def convert_json_to_python(file1, file2):
+    # Convert json to python object.
+    with open(file1) as to_check:
+        _json = json.load(to_check)
+    with open(file2) as to_schema:
+        my_schema = json.load(to_schema)
+    # Validate will raise exception if given json is not
+    # what is described in schema.
+    return _json, my_schema
+
+
+python_json, python_schema = convert_json_to_python(args.json_file, args.schema)
+check = Checker(python_json, python_schema)
+check.run_checks(check.json_object)
+objects = []
+genFile = HGenerate("tescik")
+genFile.write_to_file(args.header_name, python_json)
